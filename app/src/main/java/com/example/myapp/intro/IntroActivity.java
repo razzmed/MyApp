009c280922b94b212afa5aac590feeaf0204b108
razzmed <mederrazzakov@gmail.com>
2020-06-13 20:21:40 +0600

    add skip and next bottom

--- app/src/main/java/com/example/myapp/intro/IntroActivity.java
+++ app/src/main/java/com/example/myapp/intro/IntroActivity.java
@@ -7,11 +7,20 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.myapp.MainActivity;
 import com.example.myapp.R;
 
 public class IntroActivity extends AppCompatActivity {
+
+    Button skip;
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,18 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
         ViewPager viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
+        skip = findViewById(R.id.btn_skip);
+        next = findViewById(R.id.btn_next);
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+                preferences.edit().putBoolean("isShown", true).apply();
+                finish();
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
     }
 
     public class SectionPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/com/example/myapp/intro/IntroFragment.java
+++ app/src/main/java/com/example/myapp/intro/IntroFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapp.intro;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,8 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.myapp.MainActivity;
 import com.example.myapp.R;
 
 /**
@@ -33,19 +36,22 @@ public class IntroFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         TextView textTitle = view.findViewById(R.id.textTitle);
+
         super.onViewCreated(view, savedInstanceState);
         int pos = getArguments().getInt("pos");
         switch (pos) {
             case 0:
                 textTitle.setText("Fragment 1");
+
                 break;
             case 1:
                 textTitle.setText("Fragment 2");
+
                 break;
             case 2:
                 textTitle.setText("Fragment 3");
-                break;
 
+                break;
         }
     }
 }
